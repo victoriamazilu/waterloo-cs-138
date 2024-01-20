2e4a69836b5b6349ca83c318f231ab49762338d6
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-19 20:21:41 -0500

    Update a1q3

--- assgt1-main/a1q3/a1q3.cc
+++ assgt1-main/a1q3/a1q3.cc
@@ -1,18 +1,61 @@
-/*
-* CS138, W23, Assignment 1, Problem 3
-* Main Function
-*
-* main function is a special function,
-* which serves as the entry point for the program.
-* 
-* Try to keep it as simple as possible
-* and implement the logic of your program
-* in a1q3.cpp, so that you can write unit tests
-* for them and make sure of their expected behavior.
-*/
+#include <iostream>
+using namespace std;
+#include <vector>
 
-// no argc+argv because we aren't using them in A1
-int main() {
-    // TODO: Solve the problem!
-    return 0;
+void print( vector<string> lines){
+    for (int i = 0; i < lines.size(); i++){
+        cout << lines[i] << endl;
+    }
+}
+
+int main(){
+    int N;
+    string token;
+    vector<string> words;
+    vector<string> lines; // vector to store output
+    string currentLine = ""; // current line: starts empty
+
+    cin >> N;
+
+    if (N <= 0) {
+        cerr << "Error, line length must be positive." << endl;
+        return 1;
+    }
+
+    cin.ignore();
+    
+    while(cin>>token){
+        if (token.size()>N){ //cut it off if too long 
+            words.push_back(token.substr(0,N));
+        }
+        else{
+            words.push_back(token); // just add 
+        }
+    }
+
+
+
+    for (int i = 0; i < words.size(); i++) { // iterating through each word in words vector
+        if (currentLine.size() + words[i].size() + (currentLine.size() > 0) <= N) { // if theres space, execute
+            if(currentLine.size() > 0){ // if not empty, add space and word
+                currentLine += " ";
+            }
+            currentLine += words[i];
+        } else{ // if theres no space left, execute
+            lines.push_back(currentLine); // push it to output 
+            currentLine = words[i]; //reset 
+        }
+    }
+    if (!currentLine.empty()) { // this adds the remaining words to output even though they havent reached max`
+        lines.push_back(currentLine);
+    }
+
+    // for (int i = 0; i < lines.size(); i++) {
+    //     int spacesNeeded = N - lines[i].length();
+    //     if (lines[i].length() < N) {
+    //         lines[i] = lines[i] + string(spacesNeeded, ' ');
+    //     }
+    // }
+
+    print(lines);
 }
