5121a067ee1be1c921d5132ad957d14c686425fa
victoriamazilu <victoria.mazilu@icloud.com>
2024-03-14 16:55:39 -0400

    Update a4p3.cc

---
+++ assgt4-main/a4p3/a4p3.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cctype> // for tolower()
+#include <map>
+#include <cassert>
+#include "a5p2.h"
+
+using namespace std;
+    
+LexTree::LexTree () {
+    // TODO
+}
+
+LexTree::~LexTree() {
+    // TODO
+}
+
+bool LexTree::isValid() const {
+    // TODO
+}
+
+void LexTree::addWord (const string& s) {
+    assert(isValid()); // precondition
+    // TODO
+    assert(isValid()); // postcondition
+}
+
+bool LexTree::hasWord(const string& s) const {
+    assert(isValid()); // precondition
+    // TODO
+}
+
+string LexTree::toString() const {
+    assert(isValid()); // precondition
+    // TODO
+}
