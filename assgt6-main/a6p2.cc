1c4e78504c00867ba616f7f4b06f5d7dcf1f0be4
victoriamazilu <victoria.mazilu@icloud.com>
2024-04-14 08:22:57 -0400

    Implementation

--- assgt6-main/a6p2.cc
+++ assgt6-main/a6p2.cc
@@ -3,11 +3,23 @@
 
 using namespace std;
 
-// TODO - Implement the SmartHashTable class
-SmartHashTable::SmartHashTable() {}
+SmartHashTable::SmartHashTable() : HashTable(HashTable::DefaultSize) {}
 
-SmartHashTable::SmartHashTable(int k) {}
+SmartHashTable::SmartHashTable(int k) : HashTable(k) {}
 
 SmartHashTable::~SmartHashTable() {}
 
-int SmartHashTable::hash(string key) const {}
+// The following code was inspired by:  http://www.cse.yorku.ca/~oz/hash.html
+int SmartHashTable::hash(string key) const {
+    unsigned long hash = 5381;
+    unsigned int size = key.length();
+    unsigned int i = 0;
+    for (i = 0; i < size; i++) {
+
+        hash = ((hash << 5) + hash) + (key[i]); /* hash * 33 + c */
+    }
+
+    return hash % getTableSize();
+}
+
+
