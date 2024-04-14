47c0b38218f981ec7d1e195d8747b1d6f0b62045
victoriamazilu <victoria.mazilu@icloud.com>
2024-04-14 08:22:27 -0400

    Skeleton

---
+++ assgt6-main/a6p2.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include "a6p2.h"
+
+using namespace std;
+
+// TODO - Implement the SmartHashTable class
+SmartHashTable::SmartHashTable() {}
+
+SmartHashTable::SmartHashTable(int k) {}
+
+SmartHashTable::~SmartHashTable() {}
+
+int SmartHashTable::hash(string key) const {}
