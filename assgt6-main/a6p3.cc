f1f3368933d371b336fd4d2e07751bf44437fc11
victoriamazilu <victoria.mazilu@icloud.com>
2024-04-20 22:33:06 -0400

    Update a6p3.cc

--- assgt6-main/a6p3.cc
+++ assgt6-main/a6p3.cc
@@ -41,13 +41,68 @@ vector<string> powerset(string s) {
 
 // TODO - Implement the two overloads of scrabbleValue()
 int scrabbleValue(char c) {
- 
+    if (!isalpha(c)) {
+        cerr << "Error: Received non-alphabetical character '" << c << "'" << endl;
+        assert(false); 
+    }
+
+    c = tolower(c);
+
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'l' || c == 'n' || c == 'o' || c == 'r' || c == 's' || c == 't' || c == 'u') {
+        return 1;
+    } else if (c == 'd' || c == 'g') {
+        return 2;
+    } else if (c == 'b' || c == 'c' || c == 'm' || c == 'p') {
+        return 3;
+    } else if (c == 'f' || c == 'h' || c == 'v' || c == 'w' || c == 'y') {
+        return 4;
+    } else if (c == 'k') {
+        return 5;
+    } else if (c == 'j' || c == 'x') {
+        return 8;
+    } else { 
+        return 10;
+    }
 }
 
 int scrabbleValue(string s) {
-
+    int sum = 0;
+    for (char c : s) {
+        sum += scrabbleValue(c);  
+    } 
+    return sum;
 }
 
 string bestScrabbleWord(string letters) {
+    for (char l : letters) {
+        if (!isalpha(l)) {
+            cerr << "Error: Received non-alphabetical character '" << l << "'" << endl;
+            assert(false);
+        }
+    } 
 
+    // HashTable to check if the word is a valid Scrabble word
+    SimpleHashTable validWords;
+    ifstream file("twl-words.txt");
+    validWords.insert(file);
+    string result = "";
+
+    map<int, string> validScrabbleWords;
+    vector<string> allWords = powerset(letters);
+
+    int maxLength = -1;
+
+    for (const string& word : allWords) {
+        // Convert word to lowercase in order to use lookup
+        string wordLC = word;
+        transform(wordLC.begin(), wordLC.end(), wordLC.begin(), ::tolower);
+
+        // If the value has a higher scrabble value and is a valid word then set it as the result
+        if (scrabbleValue(word) > maxLength && validWords.lookup(wordLC)) {
+            maxLength = scrabbleValue(word);
+            result = word;
+        }
+    }
+
+    return result;
 }
