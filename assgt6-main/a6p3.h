5a095f10b029087ee0bbff5c8ed8cfd57ad445b5
victoriamazilu <victoria.mazilu@icloud.com>
2024-04-21 16:02:10 -0400

    Update a6p3.h

---
+++ assgt6-main/a6p3.h
@@ -0,0 +1,12 @@
+/* ************************************************************** */
+/*                     DO NOT MODIFY THIS FILE                    */
+/* ************************************************************** */
+#ifndef A6P3_H
+#define A6P3_H
+#include <string>
+
+int scrabbleValue(char c);
+int scrabbleValue(std::string s);
+std::string bestScrabbleWord(std::string letters);
+
+#endif
